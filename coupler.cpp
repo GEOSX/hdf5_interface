a1fe721c40ce7e66c5b3eee54b602a411c3b9b4a
Benjamin Curtice Corbett <corbett5@llnl.gov>
2019-05-13 11:16:38 -0700

    Fixed zero length read error.

--- coupler.cpp
+++ coupler.cpp
@@ -30,15 +30,18 @@ void createDataset(hid_t group, const char* name, hid_t type,
   hid_t dataset = H5Dcreate(group, name, type, dataspace, H5P_DEFAULT, 
                             H5P_DEFAULT, H5P_DEFAULT);
 
-  hid_t mem_dataspace = H5Screate_simple(1, &local_size, nullptr);
-  hid_t hyperslab = dataspace;
-  H5Sselect_hyperslab(hyperslab, H5S_SELECT_SET, &global_offset, nullptr, 
-                      &local_size, nullptr);
-  H5Dwrite(dataset, type, mem_dataspace, hyperslab, H5P_DEFAULT, data);
+  if (local_size != 0)
+  {
+    hid_t mem_dataspace = H5Screate_simple(1, &local_size, nullptr);
+    hid_t hyperslab = dataspace;
+    H5Sselect_hyperslab(hyperslab, H5S_SELECT_SET, &global_offset, nullptr, 
+                        &local_size, nullptr);
+    H5Dwrite(dataset, type, mem_dataspace, hyperslab, H5P_DEFAULT, data);
+    H5Sclose(mem_dataspace);
+  }
   
   H5Dclose(dataset);
   H5Sclose(dataspace);
-  H5Sclose(mem_dataspace);
 }
 
 /*!
@@ -147,17 +150,19 @@ void readDataset(hid_t group, const char* name, hsize_t global_offset,
     MPI_Abort(MPI_COMM_WORLD, 1);
   }
 
-  /* Create the memory and file dataspaces and do the read. */
-  hid_t mem_dataspace = H5Screate_simple(1, &n_elems, nullptr);
-  hid_t hyperslab = dataspace;
-  H5Sselect_hyperslab(hyperslab, H5S_SELECT_SET, &global_offset, 
-                      nullptr, &n_elems, nullptr);
+  if (n_elems != 0)
+  {
+    /* Create the memory and file dataspaces and do the read. */
+    hid_t mem_dataspace = H5Screate_simple(1, &n_elems, nullptr);
+    hid_t hyperslab = dataspace;
+    H5Sselect_hyperslab(hyperslab, H5S_SELECT_SET, &global_offset, 
+                        nullptr, &n_elems, nullptr);
+    H5Dread(dataset, type, mem_dataspace, hyperslab, H5P_DEFAULT, data);
+    H5Sclose(mem_dataspace);
+  }
 
-  H5Dread(dataset, type, mem_dataspace, hyperslab, H5P_DEFAULT, data);
-
-  H5Sclose(dataspace);
-  H5Sclose(mem_dataspace);
   H5Tclose(type);
+  H5Sclose(dataspace);
   H5Dclose(dataset);
 }
 
@@ -193,19 +198,22 @@ void readDataset(hid_t group, const char* name, hsize_t global_offset,
     MPI_Abort(MPI_COMM_WORLD, 1);
   }
 
-  /* Create the memory and file dataspaces and do the read. */
-  hid_t mem_dataspace = H5Screate_simple(1, &buffer_size, nullptr);
-  H5Sselect_elements(mem_dataspace, H5S_SELECT_SET, n_elems, indices);
+  if (n_elems != 0)
+  {
+    /* Create the memory and file dataspaces and do the read. */
+    hid_t mem_dataspace = H5Screate_simple(1, &buffer_size, nullptr);
+    H5Sselect_elements(mem_dataspace, H5S_SELECT_SET, n_elems, indices);
 
-  hid_t hyperslab = dataspace;
-  H5Sselect_hyperslab(hyperslab, H5S_SELECT_SET, &global_offset, 
-                      nullptr, &n_elems, nullptr);
+    hid_t hyperslab = dataspace;
+    H5Sselect_hyperslab(hyperslab, H5S_SELECT_SET, &global_offset, 
+                        nullptr, &n_elems, nullptr);
 
-  H5Dread(dataset, type, mem_dataspace, hyperslab, H5P_DEFAULT, data);
+    H5Dread(dataset, type, mem_dataspace, hyperslab, H5P_DEFAULT, data);
+    H5Sclose(mem_dataspace);
+  }
 
-  H5Sclose(dataspace);
-  H5Sclose(mem_dataspace);
   H5Tclose(type);
+  H5Sclose(dataspace);
   H5Dclose(dataset);
 }
 
